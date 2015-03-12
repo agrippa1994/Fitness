dc6be967b5c9fdaa3b7d96283c13f76dc150a01e
agrippa1994 <agrippa1994@icloud.com>
2015-03-12 17:30:32 +0100

    Erster Upload

---
+++ Kraft-Ausdauer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kraft-Ausdauer
+//
+//  Created by Mani on 12.03.15.
+//  Copyright (c) 2015 Mani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
